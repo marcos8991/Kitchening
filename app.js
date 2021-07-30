24b07a52776d4807679d26e8572dc9465464ae37
MarcosZarate <agustin_8991@outlook.com>
2021-07-29 21:27:44 -0300

    agrego estructura de home

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const express = "hola"
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+
+app.use(express.static('public'));
+
+
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'views','home.html')));
+app.listen(port,()=>console.log('server corriendo en el puerto'+ port));
